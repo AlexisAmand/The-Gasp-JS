c9741e51a80a7c48f72d1c91f6a1f7b05afade27
alexisamand <alexis.amand@gmail.com>
2021-07-06 15:51:18 +0200

    [bugfix] Cartes qui ne se retournaient plus (et réorga des fichiers)

---
+++ beta/include/options.inc.php
@@ -0,0 +1,83 @@
+<!-- Fenêtre modale pour les options -->
+    
+<div class="modal fade" id="ModalOptions" tabindex="-1" role="dialog" aria-labelledby="ModalOptionsLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered " role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="ModalOptionsLabel"><?php echo FM_OPT_TITLE; ?></h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+
+        <form name="formoptions">
+        
+            <div class="modal-body text-justify">
+
+            <p><strong><?php echo FM_OPT_BACK; ?></strong></p>
+
+            <p class="text-center">
+
+            <?php 
+            
+            /* Affichage des dos de cartes. Il suffit d'ajouter un fichier de type back-XX.png pour qu'il soit ajouté à la liste. Il faut penser à incrémenter le compteur dans la boucle for ici et dans la fonction JS ChoixDos(). */
+
+            for ($i = 1; $i<6; $i++)
+                {
+                echo '<img src="images/backs/back-0'.$i.'.png" id="'.$i.'" onclick="ChoixDos(this)" class="cartes mx-1">';
+                }
+
+            ?>
+
+            </p>
+
+            <p><strong>Terrain de jeu</strong></p>
+
+            <p class="text-center">
+
+            <?php 
+            
+            /* Affichage des fonds de jeu. Il suffit d'ajouter un fichier de type fond-XX.jpg pour qu'il soit ajouté à la liste. Il faut penser à incrémenter le compteur dans la boucle for ici et dans la fonction JS ChoixTapis(). */
+
+            for ($k = 1; $k<4; $k++)
+                {
+                echo '<img src="images/fonds/mini/fond-0'.$k.'.jpg" id="fond'.$k.'" onclick="ChoixTapis('.$k.')" class="fonds mx-1">';
+                }
+
+            ?>
+
+            </p>
+
+            <?php /* Couper ou activer le son du jeu */ ?>
+
+            <div class="custom-control custom-checkbox">
+            <script type="text/javascript">
+
+                if (sessionStorage.son) {
+                    if(sessionStorage.getItem("son") == "1") {
+                        document.write('<input type="checkbox" class="custom-control-input" id="customCheck1" checked onChange="ChangeStatut(formoptions)">');
+                    } else {
+                        document.write('<input type="checkbox" class="custom-control-input" id="customCheck1 onChange="ChangeStatut(formoptions)">');
+                    }
+                } else {
+                    document.write('<input type="checkbox" class="custom-control-input" id="customCheck1" checked onChange="ChangeStatut(formoptions)">');
+                    sessionStorage.setItem('son', "1");
+                }
+                </script>
+
+                <label class="custom-control-label" for="customCheck1">Activer le son du jeu</label>
+            </div>
+
+            </div>
+
+            <div class="modal-footer">
+                <div class="col-md-12 text-center">
+                    <button type="button" class="btn btn-primary" onclick="$('#ModalOptions').modal('hide');"><?php echo FM_OPT_SEND; ?></button>
+                </div>
+            </div>
+
+        </form>
+
+        </div>
+    </div>
+</div>
---
+++ js/thegasp.js
@@ -0,0 +1,173 @@
+/* --------------- */
+/* Nouvelle partie */
+/* --------------- */
+
+function NouvellePartie()
+	{
+	document.location.href = 'jeu.html';	
+	nbCoups = 0;
+	document.Info.coups.value = nbCoups;
+	
+	$('#ModalNouvellePartie').modal('hide');
+		
+	for (var y = 0; y < 6; y++)
+		{
+		
+		for (var x = 0; x < 6; x++)
+			{
+				document.getElementById("carte" + x.toString() + y.toString()).src = dos;
+				grille[x][y] = "O";
+
+			}
+		}
+	}
+
+/* ---------------------------------------- */
+/* Choix d'un deck dans les options de jeux */
+/* ---------------------------------------- */
+
+function mark(el) 
+	{
+
+	/* la variable dos prend comme valeur le chemin vers l'image que l'on a choisi */
+
+	dos = el.src;
+
+	/* gestion du cadre bleu autour des images dans la modale des options */
+
+	for (var a = 1; a < 7; a++)
+		{
+		document.getElementById(a).style.border = "0px solid blue";
+		}
+
+		el.style.border = "2px solid blue";
+
+	/* on change le deck dans la partie en cours */	
+
+	for (var y = 0; y < 6; y++)
+		{	
+		for (var x = 0; x < 6; x++)
+			{
+				if (grille[x][y] == "O")
+					{
+					document.getElementById("carte" + x.toString() + y.toString()).src = dos;
+					}	
+			}
+		}
+	}	
+
+/* -------------------------------------------------------------------------------- */
+/* A chaque coups, cette fonction vérifie la grille, pour voir si le joueur a gagné */
+/* -------------------------------------------------------------------------------- */
+	
+function VerificationGrille()
+	{
+	var perdu = 0;
+	for (var y = 1; y < 5; y++)
+		{			
+		for (var x = 1; x < 5; x++)
+			{
+			if (grille[x][y] == "X")
+				{
+				perdu = perdu + 1;
+				}
+			}
+		}
+	
+	if (perdu == 16)
+		{
+		$('#ModalBravo').modal('show');
+
+		$('#ModalBravo').on('shown.bs.modal', function (e) {
+			var modal = $(this)
+			modal.find('.modal-body').text('Vous avez gagné en ' + nbCoups + ' coups !')
+		  })
+		}
+
+	}
+
+/* ----------------------------------*/
+/* cette fonction retourne une carte */
+/* ----------------------------------*/
+	
+function retourne(xc, yc)
+	{
+	
+	/* test jouer un son */
+	
+	var audio = new Audio('sounds/9388.mp3');
+	audio.play();
+	
+	/* ----------------- */
+	
+	nbCoups = nbCoups + 1;
+	document.Info.coups.value = nbCoups	
+		for (var y = yc - 1; y < yc + 2; y++)
+			{
+			
+			for (var x = xc - 1; x < xc + 2; x++)
+				{
+				if (grille[x][y] == "O")
+					{
+					document.getElementById("carte" + x.toString() + y.toString()).src = "images/carte" + x.toString() + y.toString() + ".png";
+					grille[x][y] = "X";
+					}
+				else
+					{
+					document.getElementById("carte" + x.toString() + y.toString()).src = dos;
+					grille[x][y] = "O";
+					}
+				}
+			}
+
+		if (grille[xc][yc] == "O")
+			{
+			document.getElementById("carte" + xc.toString() + yc.toString()).src = "images/carte" + xc.toString() + yc.toString() + ".png";
+			grille[xc][yc] = "X";
+			}
+		else
+			{
+			document.getElementById("carte" + xc.toString() + yc.toString()).src = dos;
+			grille[xc][yc] = "O";
+			}	
+
+		VerificationGrille();	
+	}
+
+/* ---------------------------------------- */
+/* Choix d'un fond dans les options de jeux */
+/* ---------------------------------------- */
+
+/*
+function OptionWallpaper(el) 
+	{
+
+	for (var a = 11; a < 17; a++)
+		{
+		document.getElementById(a).style.border = "0px solid blue";
+		}
+
+	el.style.border = "2px solid blue";
+	$('body').css("background-image", 'url("'+el.src+'")');
+	
+	}
+*/
+	
+// Initialisation du nombre de coups
+	
+var nbCoups = 0;
+document.Info.coups.value = "0";	
+	
+// création de la grille de jeu
+var grille = new Array();
+var dos = "images/backs/back.png";
+
+// initialisation de la grille. 
+// Elle fait 9x9 mais 6x6 pourrait être suffisant.
+
+for(var i=0; i<9; i++)
+	grille[i] = new Array();
+
+for(var i=0; i<9; i++)
+	for(var j=0; j<9; j++)
+		grille[i][j] = "O";
